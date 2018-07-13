87e97cec81e02e07a00ebbc5b45b1acb8836f68e
Jessteus <mateusvanhalen@gmail.com>
2018-07-13 11:49:58 -0500

    2nd thing

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ go ("north", 82);
 go ("north", 75);
 go ("north", 95);
 go ("south", 115);
+go ("east", 25);
 
 
 // Add a condition inside the function to check if the speed is greater than 75 mph. If it is, add an additional message of SLOW DOWN!
@@ -29,3 +30,25 @@ go ("south", 115);
 
 // The car is moving forward at 82 mph
 // SLOW DOWN!
+
+//LINE BREAK LINE BREAK LINE BREAK LINE BREAK LINE BREAK
+
+// Given the following set of data about chicken birth rates, you need to filter out the objects where the birth rate value is greater than 15 per 1000 chickens for that year. Store the good years in a new array named `bestYears`.
+
+const bestYears = []
+const birthRates = [
+  { year: 1969, birthRate: 13}, 
+  { year: 1970, birthRate: 16}, 
+  { year: 1971, birthRate: 15}, 
+  { year: 1972, birthRate: 11}, 
+  { year: 1973, birthRate: 18}, 
+  { year: 1974, birthRate: 17}, 
+  { year: 1975, birthRate: 9}
+];
+
+for (currentYear of birthRates) {
+    if (currentYear.birthRate > 15) {}
+    bestYears.push(currentYear)
+}
+
+console.log(bestYears)
